0b52c59e01f63e2126243968cb125251ebe18ea0
Adam <adam.trojecki@interia.pl>
2024-02-07 15:25:44 +0100

    Buttons

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,36 @@
+import { useState } from "react";
 import NewProject from "./components/NewProject";
+import NoProjectSelected from "./components/NoProjectSelected";
 import ProjectsSidebar from "./components/ProjectsSidebar";
 
 function App() {
+  const [projectsState, setProjectState] = useState({
+    selectedProjectId: undefined,
+    projects: []
+  });
+
+  function handleStartAddProjectg(){
+    setProjectState(prevState => {
+      return {
+        ...prevState,
+        selectedProjectId: null, 
+      };
+    });
+
+  }
+
+  let content;
+
+  if (projectsState.selectedProjectId === null){
+    content = <NewProject />
+  } else if (projectsState.selectedProjectId === undefined) {
+    content = <NoProjectSelected onStartAddProject={handleStartAddProjectg} />
+  }
+
   return (
     <main className="h-screen my-8 flex">
-      <ProjectsSidebar />
-      <NewProject />
+      <ProjectsSidebar onStartAddProject={handleStartAddProjectg} />
+      {content}
     </main>
   );
 }
