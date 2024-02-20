49fdbe1e57ed851c9a3c888ee1be93125d1a0593
adask0 <adam.trojecki@interia.pl>
2024-02-20 17:52:58 +0100

    setting letters limit and fixing clear bug

--- src/components/NewTask.jsx
+++ src/components/NewTask.jsx
@@ -8,13 +8,28 @@ export default function NewTask({onAdd}) {
     }
 
     function handleClick() {
+        if(enteredTask.trim() !== ''){
         onAdd(enteredTask);
         setEnteredTask('');
+        }
+    }
+
+    function handleKeyPress(event){
+        if(event.key === 'Enter' && enteredTask.trim() !== ''){
+            onAdd(enteredTask)
+            setEnteredTask('');
+        }
     }
 
     return(
         <div className="flex items-center gap-4">
-            <input type="text" className="w-64 px-2 py-1 rounded-sm bg-stone-200" onChange={handleChange} value={enteredTask} />
+            <input type="text"
+                className="w-64 px-2 py-1 rounded-sm bg-stone-200"
+                onChange={handleChange}
+                value={enteredTask}
+                maxLength={30}
+                onKeyPress={handleKeyPress}
+            />
             <button className="text-stone-700 hover:text-stone-950" onClick={handleClick}>Add Task</button>
         </div>
     )
